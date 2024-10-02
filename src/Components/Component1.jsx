8f82bb925187756446b1d32d0bfaf3c432b94a87
humashaaa <afridakhan2121@gmail.com>
2024-10-02 20:40:08 +0600

    basic component setup

---
+++ src/Components/Component1.jsx
@@ -0,0 +1,12 @@
+
+const Component1 = () => {
+    return (
+        <div className="space-x-3 bg-slate-200 shadow-lg p-16 mt-10 w-96 h-48 mx-auto">
+            <h1 className="font-bold text-center mb-5">Component 1</h1>
+            <button className="font-semibold text-white bg-blue-500 px-4 py-2.5">Increment</button>
+            <button className="font-semibold text-white bg-rose-500 px-4 py-2.5">Increment</button>
+        </div>
+    );
+};
+
+export default Component1;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,19 @@
+import Component1 from "./Components/Component1";
+import Component2 from "./Components/Component2";
 
 const Home = () => {
     return (
         <div>
             <h1 className="font-bold text-center text-4xl">Redux</h1>
+            <div>
+                <Component1></Component1>
+                <Component2></Component2>
+
+                {/* total */}
+                <div className="space-x-3 bg-slate-200 shadow-lg p-16 mt-10 w-96 h-36 mx-auto">
+               <p className="text-center font-bold"> TOTAL : </p>
+                </div>
+            </div>
         </div>
     );
 };
